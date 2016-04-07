685e2db1e24d6fd0213509a3c12b4684ca25c041
Richard Valdivieso <rvaldivieso@masonite.com>
2016-04-07 15:08:27 -0400

    Initial test creation

--- src/test/java/com/movies/services/CustomerServiceTest.java
+++ src/test/java/com/movies/services/CustomerServiceTest.java
@@ -1,11 +1,28 @@
 package com.movies.services;
+import com.movies.CustomerRepository;
+import com.movies.model.Customer;
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
 public class CustomerServiceTest {
+
+    @Test
+    public void findById() {
+        Customer customer = new Customer();
+
+        customer.setFirstName("Richard");
+        customer.setLastName("Chamberlain");
+
+        CustomerRepository mockCustomerRepository = mock(CustomerRepository.class);
+        when(mockCustomerRepository.findOne(anyLong())).thenReturn(customer);
+
+//        Customer customer = mockCustomerRepository.findOne(1L);
+        assertThat(customer.getFirstName()).isEqualTo("Donald");
+    }
 
 }
